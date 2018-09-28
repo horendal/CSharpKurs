ca860567ebec78a64205caddd30d7698439e61ea
horendal <33148569+horendal@users.noreply.github.com>
2018-09-28 11:50:02 +0200

    Main methods added.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,6 +10,37 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            for (; ;)
+            {
+                Console.Write("Wpisz swoje imię: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Witaj, " + name);
+                Console.Write("Wpisz ile masz lat: ");
+
+                int age;
+                bool result = int.TryParse(Console.ReadLine(), out age);
+
+                if (age > 18)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Brawo. Jesteś pełnoletni, możesz pić piwo.");
+                }
+                else if (result == false)
+                {
+                    Console.WriteLine("Wprowadziłeś niepoprawny wiek.");
+                }
+                else
+                {
+                    Console.WriteLine("Możemy Ci zaoferować mleko.");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+                Console.ResetColor();
+            }
+
+
         }
     }
 }
